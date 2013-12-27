6ea6d33007b45f1f71f1aef977506270d592bf20
yarfed@yandex.ru <iddqd348>
2013-12-28 01:07:15 +0200

    commit from big comp testsetofcards added(is empty)

--- src/test/java/game/SetOfCardsTest.java
+++ src/test/java/game/SetOfCardsTest.java
@@ -1,9 +1,16 @@
 package game;
 
-import junit.framework.TestCase;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
 /**
  * Created by looser on 12/27/13.
  */
-public class SetOfCardsTest extends TestCase {
+public class SetOfCardsTest  {
+
+    @Test
+    public void testIsEmpty() {
+        SetOfCards setOfCards = new SetOfCards();
+        assertFalse(setOfCards.isEmpty());
+    }
 }
